29b3dca19f7c95c5711ef386a4e537c3f99d48f3
SzymonKubica <szymo.kubica@gmail.com>
2023-04-03 10:53:40 +0200

    Implemented nice battery warnings.

--- battery.sh
+++ battery.sh
@@ -4,6 +4,14 @@ state=$(upower -i $(upower -e | grep 'BAT') | grep -E "state" | awk '{print $2}'
 
 charge_level="${percentage%%[%]*}"
 
+if [[ $((charge_level)) -lt 5 && "$state" == "discharging" ]]
+then
+  notify-send "Battery level critical: ${charge_level}% Please charge immediately!"
+elif [[ $((charge_level)) -lt 40 && "$state" == "discharging" ]]
+then
+  notify-send -a "sway" "Battery level is low: ${charge_level}% Please connect charger."
+fi
+
 battery_icon=""
 
 if [[ $((charge_level)) -gt 90 ]]
